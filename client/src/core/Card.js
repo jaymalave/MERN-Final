d00d67be17bac2ca3762d22200ebdfb9decd57e9
Sushil <mauryasushil045@gmail.com>
2022-11-28 16:38:45 +0530

    feat: updated ui

--- client/src/core/Card.js
+++ client/src/core/Card.js
@@ -40,6 +40,7 @@ const useStyles = makeStyles((theme) => ({
     height: '100%',
     display: 'flex',
     flexDirection: 'column',
+    borderRadius: "25px",
   },
   cardMedia: {
     paddingTop: '56.25%', // 16:9
